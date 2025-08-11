c77db314c4eacea943923e72899872ee50021984
SazaO_o <121901716+Saza-dev@users.noreply.github.com>
2025-08-11 16:45:36 +0530

    Assistant

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+from pathlib import Path
+from langchain.agents import create_sql_agent
+from langchain.sql_database import SQLDatabase
+from langchain.agents.agent_types import AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from sqlalchemy import create_engine
+import sqlite3 
+from langchain_ollama import ChatOllama
+
+st.set_page_config(page_title="Langchain: Chat with SQL DB")
+st.title("Chat with SQL DB")
+
+INJECTION_WARNING = """
+SQL agent can be vulnerable to prompt injection. 
+Use a DB role with limited permissions. 
+Read more [here](https://python.langchain.com/docs/security).
+"""
+LOCALDB="USE_LOCALDB"
+MYSQL="USE_MYSQL"
+
+radio_opt=["USE SQLLite3 Database - Student.db","Connect to your SQL Database"]
+
+selected_opt = st.sidebar.radio(label="Choose the DB which you want to chat",options=radio_opt)
+
+if radio_opt.index(selected_opt)==1:
+    db_uri = MYSQL
+    mysql_host = st.sidebar.text_input("Provide MySQL Host")
+    mysql_user = st.sidebar.text_input("MYSQL User")
+    mysql_password=st.sidebar.text_input("MYSQL password",type="password")
+    mysql_db = st.sidebar.text_input("MySQL database")
+
+else: 
+    db_uri = LOCALDB
+
+if not db_uri:
+    st.info("Please Enter the database information and url")
+
+llm = ChatOllama(model="llama3.1",streaming=True)
+
+def configure_db(db_uri,mysql_host=None,mysql_user=None,mysql_password=None,mysql_db=None):
+    if db_uri == LOCALDB:
+        dbFilepath = (Path(__file__).parent/"student.db").absolute()
+        print(dbFilepath)
+        creator = lambda: sqlite3.connect(f"file:{dbFilepath}?mode=ro",uri=True)
+        return SQLDatabase(create_engine("sqlite:///",creator=creator))
+    elif db_uri == MYSQL:
+        if not (mysql_db and mysql_host and mysql_password and mysql_user):
+            st.error("Please provide all MySQL connection details.")
+            st.stop()
+        return SQLDatabase(create_engine(f"mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}"))
+
+if db_uri == MYSQL: 
+    db = configure_db(db_uri,mysql_host,mysql_user,mysql_password,mysql_db)
+else: 
+    db = configure_db(db_uri)
+
+# toolkit
+toolkit = SQLDatabaseToolkit(db=db,llm=llm)
+
+agent = create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION
+)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+    st.session_state["messages"] = [{"role":"assistant","content":"How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+user_query = st.chat_input(placeholder="Ask anything from the database")
+
+if user_query:
+    st.session_state.messages.append({"role":"user","content":user_query})
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        streamlit_callback = StreamlitCallbackHandler(st.container())
+        response = agent.run(user_query,callbacks=[streamlit_callback])
+        st.session_state.messages.append({"role":"assistant","content":response})
+        st.write(response)
